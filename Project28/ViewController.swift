0eb800556a54893cabb206ff85a234bcf55726a1
Diego Pérez <diegodevelops@gmail.com>
2023-10-24 19:05:15 -0400

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Diego A. Perez Pares on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project28UITests/Project28UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project28UITestsLaunchTests.swift
+//  Project28UITests
+//
+//  Created by Diego A. Perez Pares on 10/24/23.
+//
+
+import XCTest
+
+final class Project28UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
